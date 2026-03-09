419c4d54a1c5b462ea3b5c491b1442bf38173b82
fotoshootflat-debug <fotoshoot.flat@googlemail.com>
2026-03-09 19:00:22 +0100

    Delete pages/3_ReportGenerator.py

--- pages/3_ReportGenerator.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.title("📄 Intelligence Report Generator")
-
-if st.button("Generate Briefing"):
-    st.write("Situation Overview")
-    st.write("Economic Context")
-    st.write("Security Risk")
-    st.write("Future Outlook")
